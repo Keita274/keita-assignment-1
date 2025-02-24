d566c9d7612178bddbefccccdcdac21e69c511c4
Keita274 <benjaminkeita073@gmail.com>
2025-02-24 02:20:51 -0600

    Update keita simple.c

--- keita simple.c
+++ keita simple.c
@@ -1,6 +1,6 @@
 /*
- Student Name: Raymond Ndoro Tsuma 
- Student Reg No: D33-2402-2023
+ Student Name: Benjamin keita
+ Student Reg No: D33-0270-2022
  Course Unit: BMS 201
  Student Year & Sem: 2.2
  Test Quiz:  SIMPLE INTEREST 
